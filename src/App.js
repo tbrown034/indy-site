14cf0ccde79019ada9a89ff0c5f1b10f3fbc19d1
tbrown034 <tbrown034@gmail.com>
2023-01-03 15:28:37 -0600

    added Home component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Home/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+
+    return (
+        <div className="homeContain">
+            <h1>Indy Pup</h1>
+            <h2>A Photo Gallery of the Pup</h2>
+            <p>Made by Trevor Brown</p>
+        </div>
+    )
+}
+
+export default Home;
