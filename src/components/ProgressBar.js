e709807938f11fec290e8985a2caeb2048e788aa
tbrown034 <tbrown034@gmail.com>
2023-01-03 20:11:17 -0600

    progress bar works frontend

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "./hooks/useStorage";
 
 const ProgressBar = ({file, setFile}) => {
     const {url, progress } = useStorage(file);
+
+    useEffect (() => {
+        if (url) {
+            setFile(null);
+        }
+    }, [url, setFile]
+    
+    )
+
+
     console.log(progress, url)
     return (
     <div className="progressBar" style={{width: progress + '%'}} >Progress</div>
